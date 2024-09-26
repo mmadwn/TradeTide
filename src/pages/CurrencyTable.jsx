6ec775b39fb1727788a3662bff3914e960146065
Ridwan <49033429+mmadwn@users.noreply.github.com>
2024-09-26 10:00:29 +0700

    docs: comment console.log

--- src/pages/CurrencyTable.jsx
+++ src/pages/CurrencyTable.jsx
@@ -4,7 +4,7 @@ import CurrencyTableUI from '../components/CurrencyTableUI'; // Ensure this path
 const CurrencyTable = ({ isDarkTheme }) => { // Accept the theme prop
     const apiKey = import.meta.env.VITE_CURRENCY_API_KEY; 
     const { rates, error, loading } = useCurrencyRates(apiKey); // Destructure error and loading
-    console.log(rates, error, loading)
+    // console.log(rates, error, loading)
     return <CurrencyTableUI rates={rates} error={error} loading={loading} isDarkTheme={isDarkTheme} />; // Pass theme state
 };
 
